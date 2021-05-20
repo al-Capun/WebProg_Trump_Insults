df5870e543db236b8cadcd5112dd38fc3a563486
LetiziaReinhardt <61312731+LetiziaReinhardt@users.noreply.github.com>
2021-05-20 11:39:52 +0200

    Add files via upload
    Balkendiagramm

--- WebProg_Trump_Insults-main/Code/main.py
+++ WebProg_Trump_Insults-main/Code/main.py
@@ -8,7 +8,12 @@ import json
 import time
 import random
 
-app = Flask("flask-music")
+#Import von Elementen für Balkendiagramm
+from collections import Counter, OrderedDict
+
+app = Flask("flask-trump")
+#Bugfix für die fehldende Sortierung in main.py durch Flask --> Quelle: https://github.com/pallets/flask/issues/974
+app.config["JSON_SORT_KEYS"] = False 
 
 def load_tweets():
 	tweets_path = os.path.join(
@@ -32,6 +37,27 @@ def index():
 @app.route('/random_tweet')
 def random_tweet():
 	return get_random_tweet()
+
+#Funktion für Balkendiagramm
+#pro Tweet aus der Liste full_tweets wird das Tweet-Element mit der Kennung "insult" zur neuen Liste insult_list hinzugefügt
+#mittels Counter werden gleiche Elemente in der Liste insult_list gezählt, erhalten dann den Wert, wie oft diese vorkommen und werden einmalig als einmaliger Eintrag in der Liste zusammengefasst
+#mittels OrderedDict wird die insult_list nach den meist genannten Insults in die Liste sorted_insult_list übergeben, die Variable count definiert, wie viele Werte in diese Liste geschrieben werden sollen
+def get_bd_insult_words(count):
+	insult_list = []
+	full_tweets = load_tweets()
+	for tweet in full_tweets:
+		insult_list.append(tweet["insult"])
+	insult_list = Counter(insult_list)
+	sorted_insult_list = OrderedDict(insult_list.most_common(count))
+	#print(sorted_insult_list)
+	return sorted_insult_list
+
+#die Variable count wird hier mit einem int-Wert aus dem Formular befüllt
+#zurückgegeben wird das Resultat der Funktion get_bd_insult_words(count)
+@app.route('/keywords')
+def keywords():
+	count = int(request.args.get("count"))
+	return get_bd_insult_words(count)
 
 
 if __name__ == "__main__":
--- WebProg_Trump_Insults-main/Code/static/js/main.js
+++ WebProg_Trump_Insults-main/Code/static/js/main.js
@@ -4,6 +4,46 @@ let random_insult = $("#random_insult");
 let date = $("#date");
 let target = $("#target");
 let tweet_nr = $("#tweet-nr");
+//Balkendiagramm Variablen, welche im HTML Code eingesetzt werden
+let updateChartForm = $("#updateChartForm")
+let keywordCountSelect = $("#keywordCountSelect")
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'max number of Insult',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fixedStepSize: 1
+                }
+            }]
+        }
+    }
+})
 
 // Click Handler für Random Button registrieren.
 // in "click" ist eine Funktion gespeichert.
@@ -19,6 +59,59 @@ randomTweetButton.click(function (event) {
     })
 })
 
+//Balkendiagramm
+//updateChartForm setzt die Variable count, welche in der JS-Funkiotn keywordsRequest(count) und der Pythonfunktion get_bd_insult_words(count) wiederverwendet wird
+//durch event.preventDefault, wird das standardmässige Verhalten des Submitbuttons unterbunden
+//Variable count wird ein Wert mitgegeben
+//JS-Funktion keywordsRequest(count) wird aufgerufen
+updateChartForm.submit(function (event) {
+    event.preventDefault()
+    let count = keywordCountSelect.val()
+    //console.log(count)
+    keywordsRequest(count)
+})
+
+//Funktion füllt flexibel das Balkendiagramm mit entsprechenden Daten ab
+//angepasst werden die Variablen labels und data, welche Daten auf keywordsRequest(count) erhalten
+//mittels myChart.update() wird dies initialisiert
+function updateChart(labels, data) {
+    myChart.data.labels = labels
+    myChart.data.datasets = [{
+        label: 'max number of single Insults',
+        data: data,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ]
+    }]
+    myChart.update()
+}
+
+//NProgress ist lediglich auf Aesthetikgründen, da dies einen Ladebalken zeigt, sobald die Funktion keywordsRequest(count) ausgeführt wird
+//mittels der funktion wird durch die Variable result den Variablen labels und data die keys (Name des Insults) und die values (Häufigkeit des Insults) mitgegeben
+//durch aufrufen der Funktion updateChart und mitgeben der Variablen labels und data, wird das Chart angepasst
+function keywordsRequest(count) {
+    NProgress.start();
+    $.get(`/keywords?count=${count}`, function (result) {
+        let labels = Object.keys(result)
+        let data = Object.values(result)
+        //console.log(labels)
+        //console.log(data)
+        updateChart(labels, data)
+        NProgress.done();
+    })
+}
+
+//wurde die Funktion keywordsRequest(count) noch nicht aufgerufen, wird ein Standardwert von 5 mitgegeben, so werden default immer 5 Insults in der Grafik angezeigt
+keywordsRequest(5)
+// Balkendiagramm Ende
+
+
+//Wordcloud
 $(document).ready(function()
 {
 
